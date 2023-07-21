ac7a4f16b2768eda4e1a51d1e00998068803c66f
dzapeka <douson@gmail.com>
2023-07-21 23:50:32 +0200

    implement Task 2 - video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const VIMEO_PLAYER_CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = localStorage.getItem(VIMEO_PLAYER_CURRENT_TIME_KEY) || 0;
+player.setCurrentTime(currentTime);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(seconds => {
+      localStorage.setItem(VIMEO_PLAYER_CURRENT_TIME_KEY, seconds);
+    });
+  }, 1000)
+);
